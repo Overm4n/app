b5487130c04ca117eeee4767257aaec23f11425f
Overm4n <51816528+Overm4n@users.noreply.github.com>
2020-07-16 19:28:20 +0300

    Пытаюсь записать избранное

--- src/app/components/game-list/game-list.component.ts
+++ src/app/components/game-list/game-list.component.ts
@@ -16,16 +16,29 @@ export class GameListComponent implements OnInit {
     { num: 1, name: 'desc' },
   ];
   selectSort = this.sorting[0];
+  // Favorite: new Map<string, string>;
+  Favorite: MyType;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.http.get('assets/games.json').subscribe((data: any) => {
       this.collectionSize = data.games.length;
-
+      
       for (const [index, value] of data.games.entries()) {
         this.gamesList.push(value);
       }
+
+      this.gamesList.sort(function (a, b) {
+        if (a.Name.en > b.Name.en) {
+          return 1;
+        }
+        if (a.Name.en < b.Name.en) {
+          return -1;
+        }
+        // a должно быть равным b
+        return 0;
+      })
     });
   }
 
@@ -33,7 +46,50 @@ export class GameListComponent implements OnInit {
 
   changeSort(){
     console.log(this.selectSort['name']);
+    if(this.selectSort['name'] == 'ASC'){
+      this.gamesList.sort(function (a, b) {
+        if (a.Name.en > b.Name.en) {
+          return 1;
+        }
+        if (a.Name.en < b.Name.en) {
+          return -1;
+        }
+        // a должно быть равным b
+        return 0;
+      })
+    }else{
+      this.gamesList.sort(function (a, b) {
+        if (b.Name.en > a.Name.en) {
+          return 1;
+        }
+        if (b.Name.en < a.Name.en) {
+          return -1;
+        }
+        // a должно быть равным b
+        return 0;
+      })
+    }
+  }
+
+  addFavorite(game){
+    
+
+    // this.Favorite.push(game.Name.en)
+    this.Favorite.id=game.ID
+    // Favorite.
+    // localStorage
+    // localStorage.setItem('Favorite', this.Favorite);
+    console.log( this.Favorite, game.ID, game.Name.en);
     
   }
 
+}
+
+
+interface fav {
+  [key: string]: string | boolean | number;
+}
+
+interface MyType {
+  [key: string]: any
 }
