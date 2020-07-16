bb8a7a6c27ddd06f1bb1b667582c73c37a2a70c1
Overm4n <51816528+Overm4n@users.noreply.github.com>
2020-07-16 13:35:02 +0300

    pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,8 @@ import { FilterComponent } from './components/filter/filter.component';
 import { GameListComponent } from './components/game-list/game-list.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatTableModule } from '@angular/material/table';
+
+
 
 @NgModule({
   declarations: [
@@ -27,8 +27,7 @@ import { MatTableModule } from '@angular/material/table';
     AppRoutingModule,
     NgbModule,
     HttpClientModule,
-    BrowserAnimationsModule,
-    MatTableModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
